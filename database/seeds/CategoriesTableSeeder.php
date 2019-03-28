2272ed7499b574ef5293ce8e768162410bec25e8
gitoshh <karani.gitonga.godwin@gmail.com>
2019-03-28 16:18:13 +0300

    Modify travis files

---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        DB::table('categories')->insert([
+            'categoryName'  => 'All',
+            'nodeID'        => 1,
+        ]);
+    }
+}
